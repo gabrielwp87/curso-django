55fdc90671c1d1291ea6d632ac1bc7c775f2bffc
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-04 11:20:18 -0300

    ajuste deploy 3

--- pypro/settings.py
+++ pypro/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Configuração de ambiente de desenvolvimento
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Varávies destinadas a upload de arquivos, caso o site permita
@@ -151,7 +151,7 @@ AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 if AWS_ACCESS_KEY_ID:
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControel': 'max-age=86400', }
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = False
