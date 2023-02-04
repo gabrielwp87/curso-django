d3217a55082fc00d51a995678743882ea39c57aa
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-04 20:01:13 -0300

    retorno ao pós instalado sentry mais / após STATIC_URL

--- pypro/urls.py
+++ pypro/urls.py
@@ -1,4 +1,5 @@
 """pypro URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -12,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.conf import settings
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path  # , include
+from django.urls import path, include
 
 from pypro.base.views import home
 
@@ -23,8 +24,8 @@ urlpatterns = [
     path('', home),
 ]
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns.append(
-#         path('__debug__', include(debug_toolbar.urls))
-#     )
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__', include(debug_toolbar.urls))
+    )
