bc1fe0c56dad7cac1ff984233b8a695fdd3ef221
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-04 11:11:51 -0300

    ajuste deploy 2

--- pypro/settings.py
+++ pypro/settings.py
@@ -187,7 +187,6 @@ if SENTRY_DSN:
         dsn=SENTRY_DSN, integrations=[DjangoIntegration()]
     )
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
