e4345ffd7cd61553bef256bd3e9dac616378f867
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-04 11:11:51 -0300

    ajuste deploy 2

--- pypro/settings.py
+++ pypro/settings.py
@@ -186,7 +186,6 @@ if SENTRY_DNS:
     sentry_sdk.init(
         dsn=SENTRY_DNS, integrations=[DjangoIntegration()])
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
