93b8e05eef7d632f90ecbb0da725ee98eeff7c4c
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-09 10:00:10 -0300

    criado django_assertions e teste para o title
    close #52

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,13 @@
-from multiprocessing.connection import Client
+# from multiprocessing.connection import Client
+from django.test import Client
+from pypro.django_assertions import assert_contains
 
 
 def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Python Pro</title>')
