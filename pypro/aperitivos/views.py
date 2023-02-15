db47bc7f9ddf0a67fbfae99cf468dc09734f8615
gabrielpiazenski@gmail.com <Gp231187!>
2023-02-15 11:39:40 -0300

    criado contextos de template
    close #66

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'youtube_id': 'watch?v=b899h0lNd7U'}
+    return render(request, 'aperitivos/video.html', context={'video':video})
