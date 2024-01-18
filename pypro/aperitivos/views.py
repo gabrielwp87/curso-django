49e5564a95c92dd2e205ed1e24624d856584a65b
gabriel <gabrielpiazenski@gmail.com>
2024-01-18 15:26:31 -0300

    video passado como variável de acordo com o slug
    close #15

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 def video(resquest, slug):
-    video={'titulo':'Aperitivo: Motivação', 'vimeo_id':'799826779?h=be5ee2d29c'}
-    return render(resquest, 'aperitivos/video.html', context={'video':video})
+    videos = {
+        'motivacao': {'titulo': 'Aperitivo: Motivação', 'vimeo_id': '799826779?h=be5ee2d29c'},
+        'tempo-para-ser-programador': {'titulo': 'Tempo para ser programador', 'vimeo_id': '799826679?h=728cbbb571'}
+    }
+    video = videos[slug]
+    return render(resquest, 'aperitivos/video.html', context={'video': video})
