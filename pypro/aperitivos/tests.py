fdd9b68781a785a72ee11428d605b71d7858030a
gabriel <gabrielpiazenski@gmail.com>
2024-01-18 14:54:35 -0300

    Criada herança de templates para evitar duplicação de cdóigo
    close #13

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Aperitivo: Motivação</h1>')
+    assert_contains(resp, 'Aperitivo: Motivação')
 
 
 def test_conteudo_video(resp):
