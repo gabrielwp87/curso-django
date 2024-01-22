c7900375d2d94c82d2219170098e69a40fd7facd
gabriel <gabrielpiazenski@gmail.com>
2024-01-22 15:59:10 -0300

    Subistituida a forma de armazenamento dos dados dos videos, de dct de dct para classe
    close #23

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '799826779?h=be5ee2d29c'},
-    {'slug': 'tempo-para-ser-programador', 'titulo': 'Tempo para ser programador', 'vimeo_id': '799826679?h=728cbbb571'}
+    Video('motivacao', 'Video Aperitivo: Motivação', '799826779?h=be5ee2d29c'),
+    Video('tempo-para-ser-programador', 'Tempo para ser programador', '799826679?h=728cbbb571'),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(resquest):
@@ -13,6 +21,5 @@ def indice(resquest):
 
 
 def video(resquest, slug):
-
     video = videos_dct[slug]
     return render(resquest, 'aperitivos/video.html', context={'video': video})
