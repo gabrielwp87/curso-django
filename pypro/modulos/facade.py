9b0bd1437d14210fef07a2e16dc73f425d4ee6ae
gabriel <gabrielpiazenski@gmail.com>
2024-02-01 12:47:33 -0300

    Adicionado templates
    para os links dos módulos listados
    
    close #63

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pypro.modulos.models import Modulo
+from pypro.modulos.models import Modulo, Aula
 
 
 def listar_modulos_ordenados() -> List[Modulo]:
@@ -11,9 +11,13 @@ def listar_modulos_ordenados() -> List[Modulo]:
     return list(Modulo.objects.order_by('order').all())
 
 
-def encontrar_modulo(slug:str) -> Modulo:
+def encontrar_modulo(slug: str) -> Modulo:
     return Modulo.objects.get(slug=slug)
 
 
-def listar_aulas_de_modulo_ordenadas(modulo:Modulo):
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
     return list(modulo.aula_set.order_by('order').all())
+
+
+def encontrar_aula(slug):
+    return Aula.objects.get(slug=slug)
