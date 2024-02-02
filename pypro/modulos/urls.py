712bf1f22531b704208adefe2bc9a21db691aa73
gabriel <gabrielpiazenski@gmail.com>
2024-02-02 17:37:38 -0300

    Criado o template do indice
    Indice referente aos módulos e suas respectivas aulas
    
    Criado os testes respectivos
    
    Otimizado query
    
    Aulas com ordenação garantidas dentro dos seus módulos
    
    close #73

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -23,4 +23,5 @@ app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
     path('aula/<slug:slug>', views.aula, name='aula'),
+    path('', views.indice, name='indice'),
 ]
